93d0286871aee9be1ee4ce59eb530a22d9eeefd3
itzfeltrin <pedrofeltrin45@yahoo.com>
2021-09-03 13:39:10 -0300

    feat: finish base pet crud

--- src/routes/pet.js
+++ src/routes/pet.js
@@ -4,15 +4,32 @@ const petServices = require('../services/pet');
 
 const router = express.Router();
 
-const {_getAll, _create} = petServices();
+const {
+    _getAll,
+    _getOne,
+    _createOne,
+    _updateOne,
+    _deleteAll,
+    _deleteOne
+} = petServices();
 
 router.get('/', async (req, res) => {
     try {
         const data = await _getAll();
 
-        res.json(data);
+        res.status(200).json(data);
     } catch (err) {
-        res.status(500).send("Something went wrong internally");
+        res.status(err.status).send(err);
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const data = await _getOne(req.params.id);
+
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(err.status).send(err);
     }
 })
 
@@ -20,11 +37,44 @@ router.post('/', async (req, res) => {
     try {
         const values = req.body;
 
-        const data = await _create(values);
+        await _createOne(values);
 
-        res.json(data);
+        res.status(201).json({message: "Registro criado com sucesso"});
     } catch (err) {
-        res.status(500).send("Something went wrong internally");
+        res.status(err.status).send(err);
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const values = req.body;
+
+        await _updateOne(req.params.id, values);
+
+        res.status(201).json({message: "Registro atualizado com sucesso"});
+    } catch (err) {
+        res.status(err.status).send(err);
+    }
+});
+
+router.delete('/', async (req, res) => {
+    try {
+        await _deleteAll();
+
+        res.status(200).json({message: "Todos os registros de pet foram deletados com sucesso"});
+    } catch (err) {
+        res.status(err.status).send(err);
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        await _deleteOne(req.params.id);
+
+        res.status(200).json({message: "Registro deletado com sucesso"});
+    } catch (err) {
+        console.log(err);
+        res.status(err.status).send(err);
     }
 })
 
--- src/services/pet.js
+++ src/services/pet.js
@@ -1,4 +1,5 @@
 const driver = require('../db');
+const {serviceErrorHandler} = require('../utils/helpers');
 
 const session = driver.session();
 
@@ -8,28 +9,112 @@ const _getAll = async () => {
 
         const arr = result.records.map((el) => ({
             id: el._fields[0].identity.low,
-            name: el._fields[0].properties.name,
+            ...el._fields[0].properties
         }));
 
         return arr;
     } catch (err) {
-        throw err;
+        throw serviceErrorHandler(err);
     }
 };
 
-const _create = async (values) => {
+const _getOne = async (id) => {
     try {
-        const result = await session.run("CREATE (a:Pet {name: $name})", {
-            name: values.name,
-        });
+        const result = await session.run(`MATCH (p:Pet) WHERE ID(p) = ${id} RETURN p`);
 
-        return result;
+        if (!result.records[0]) {
+            throw {
+                status: 404,
+                message: 'Registro não encontrado'
+            }
+        }
+
+        const obj = {
+            id: result.records[0]._fields[0].identity.low,
+            ...result.records[0]._fields[0].properties,
+        }
+
+        return obj;
     } catch (err) {
-        throw err;
+        throw serviceErrorHandler(err);
     }
 };
+
+// species
+// 0 = DOG
+// 1 = CAT
+
+// gender
+// 0 = MALE
+// 1 = FEMALE
+
+const _createOne = async (values) => {
+    try {
+        await session.run(`CREATE (:Pet {
+            name: $name,
+            species: $species,
+            breed: $breed,
+            gender: $gender,
+            age: $age,
+            adopted: $adopted,
+            images: $images
+        })`, values);
+    } catch (err) {
+        throw serviceErrorHandler(err);
+    }
+};
+
+const _updateOne = async (id, values) => {
+    try {
+        let query = `MATCH (p:Pet) WHERE ID(p) = ${id} SET`
+
+        Object.keys(values).forEach(key => {
+            query += ` p.${key} = \$${key},`
+        })
+
+        query = query.substring(0, query.length - 1);
+
+        const result = await session.run(query, values);
+
+        if (result.summary.counters._stats.propertiesSet === 0) {
+            throw {
+                status: 404,
+                message: 'Registro não encontrado'
+            }
+        }
+    } catch (err) {
+        throw serviceErrorHandler(err);
+    }
+};
+
+const _deleteAll = async () => {
+    try {
+        await session.run("MATCH (p:Pet) DELETE p");
+    } catch (err) {
+        throw serviceErrorHandler(err);
+    }
+}
+
+const _deleteOne = async (id) => {
+    try {
+        const result = await session.run(`MATCH (p:Pet) WHERE ID(p) = ${id} DELETE p`);
+
+        if (result.summary.counters._stats.nodesDeleted === 0) {
+            throw {
+                status: 404,
+                message: 'Registro não encontrado'
+            }
+        }
+    } catch (err) {
+        throw serviceErrorHandler(err);
+    }
+}
 
 module.exports = () => ({
     _getAll,
-    _create,
+    _getOne,
+    _createOne,
+    _updateOne,
+    _deleteAll,
+    _deleteOne,
 })
